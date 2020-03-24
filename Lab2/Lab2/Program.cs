33050bc41ad605ccd76b8c6400161847cebcf325
julio-Cortes <jicortes1@miuandes.cl>
2020-03-24 17:18:02 -0300

    infodone

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cancion Zoom = new Cancion();
+            Zoom.Name = "Zoom";
+            Zoom.Album = "Me veras Volver";
+            Zoom.Artist = "Soda Stereo";
+            Zoom.Genre = "Rock Latino";
+            Console.WriteLine(Zoom.Informacion());
+        }
+    }
+}
