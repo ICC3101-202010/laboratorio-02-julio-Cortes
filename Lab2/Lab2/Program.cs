60a5775c23fec7ea7e7a61531d690f9dc2a5075d
julio-Cortes <jicortes1@miuandes.cl>
2020-03-24 22:57:01 -0300

    incremento2sintest

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -18,22 +18,26 @@ namespace Lab2
             while (stopper!="3")
             {
                 Console.WriteLine("\nque funcion desea utilizar?\n");
-                Console.WriteLine("(1)Agregar Cancion \n(2)VerCanciones \n(3)Salir del programa");
+                Console.WriteLine("(1)Agregar Cancion \n(2)VerCanciones \n(3)Ver canciones por criterio \n(4)Salir del programa");
                 stopper= Console.ReadLine();
                 switch (stopper)
                 {
                     case "1":
                         Console.WriteLine("Ingrese el nombre de la cancion: ");
                         string nameofsong = Console.ReadLine();
+
                         Console.WriteLine("Ingrese el nombre del album: ");
                         string nameofalbum = Console.ReadLine();
+
                         Console.WriteLine("Ingrese el nombre del artista: ");
                         string nameofartist = Console.ReadLine();
+
                         Console.WriteLine("Ingrese el nombre del genero: ");
                         string nameofgenre = Console.ReadLine();
 
                         Cancion cancion = new Cancion(nameofsong, nameofalbum, nameofartist, nameofgenre);
                         sp.AgregarCancion(cancion);
+
                         if (sp.AgregarCancion(cancion))
                         {
                             Console.WriteLine("La Cancion se anadio correctamente");
@@ -47,7 +51,15 @@ namespace Lab2
                     case "2":
                         sp.VerCancion();
                         break;
+
                     case "3":
+                        Console.WriteLine("Ingrese el criterio a utilizar: ");
+                        string criterio = Console.ReadLine();
+                        Console.WriteLine("Ingrese el valor del criterio: ");
+                        string valor = Console.ReadLine();
+                        sp.CancionesPorCriterio(criterio, valor);
+                        break;
+                    case "4":
                         break;
                     default:
                         Console.WriteLine("Ingrese una opcion valida");
